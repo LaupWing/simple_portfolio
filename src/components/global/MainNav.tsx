0d0b1476ca6b03824a68263ae6b51b781c28bd51
LaupWing <loc_strict@hotmail.com>
2023-02-27 10:42:41 +0100

    added customf ont and link

--- src/components/global/MainNav.tsx
+++ src/components/global/MainNav.tsx
@@ -1,23 +1,47 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { FC } from "react"
 
 export const MainNav = () => {
    return (
-      <header className="w-full flex justify-between mx-auto max-w-4xl">
+      <header className="w-full text-sm flex py-4 items-center justify-between mx-auto max-w-4xl">
          <div>Logo</div>
-         <nav className="py-4 space-x-4">
+         <nav className="space-x-4">
             <Link href={"/"}>
                Home
             </Link>
-            <Link href={"/"}>
+            <Link href={"/work"}>
                Work
             </Link>
-            <Link href={"/"}>
+            <Link href={"/about"}>
                About
             </Link>
-            <Link href={"/"}>
+            <Link href={"/contact"}>
                Contact
             </Link>
          </nav>
       </header>
+   )
+}
+
+interface NavLinkProps {
+   href: string
+   text: string
+}
+
+const NavLink:FC<NavLinkProps> = ({
+   href,
+   text
+}) => {
+   const router = useRouter()
+   const isActive = router.pathname === href
+
+   return (
+      <Link 
+         href={href}
+         className={`tracking-tighter ${isActive ? "font-bold" : ""}`}
+      >
+         {text}
+      </Link>
    )
 }
