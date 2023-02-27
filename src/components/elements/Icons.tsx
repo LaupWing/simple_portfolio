714f1ac93d6fdf6f4e04a8e2c6f636638ee2b534
LaupWing <loc_strict@hotmail.com>
2023-02-27 15:33:19 +0100

    icons

--- src/components/elements/Icons.tsx
+++ src/components/elements/Icons.tsx
@@ -12,3 +12,18 @@ export const IconInstagram:IconType = (props) => {
    return <AiFillInstagram {...props}/>
 } 
 
+export const IconLinkedIn:IconType = (props) => {
+   return <AiFillLinkedin {...props}/>
+} 
+
+export const IconDribbble:IconType = (props) => {
+   return <AiFillDribbbleCircle {...props}/>
+} 
+
+export const IconTwitter:IconType = (props) => {
+   return <AiOutlineTwitter {...props}/>
+} 
+
+export const IconArrow:IconType = (props) => {
+   return <BsFillArrowUpRightCircleFill {...props}/>
+} 
