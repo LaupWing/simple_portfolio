3963f8d259d3528e2db007ffb7b3db7a405d94d6
LaupWing <loc_strict@hotmail.com>
2023-02-27 15:31:24 +0100

    Instagram

--- src/components/elements/Icons.tsx
+++ src/components/elements/Icons.tsx
@@ -1,1 +1,14 @@
-export {}
+import { IconType } from "react-icons"
+import {
+   AiFillInstagram,
+   AiFillLinkedin,
+   AiFillDribbbleCircle,
+   AiOutlineTwitter
+} from "react-icons/ai"
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs"
+
+
+export const IconInstagram:IconType = (props) => {
+   return <AiFillInstagram {...props}/>
+} 
+
