b3658f99fc1414e3984806c1fdb456d84a1ce000
LaupWing <loc_strict@hotmail.com>
2023-03-01 15:26:57 +0100

    udpate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 import Head from "next/head"
 import { MainNav } from "~/components/global"
 import data from "../../data.json"
 import { ShortIntro } from "~/components/sections"
 import { ProjectCard } from "~/components/cards"
+import { Carousel } from "react-responsive-carousel"
 
 export default function Home() {
    return (
@@ -21,13 +23,19 @@ export default function Home() {
             <main className="w-full max-w-4xl mx-auto grid grid-cols-1 gap-6">
                <ShortIntro />
                <section className="grid grid-cols-2 gap-6">
-                  <div className="col-span-2 h-40 relative overflow-hidden rounded-2xl">
+                  <div className="col-span-2 h-40 relative overflow-hidden rounded-2xl flex">
                      <img 
                         className="absolute inset-0 object-cover -z-10" 
                         src="https://images.pexels.com/photos/249798/pexels-photo-249798.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                         alt="github" 
                      />
                      <div className="inset-0 absolute bg-white/40 backdrop-blur-sm -z-10"/>
+                     <Carousel className="flex-1 h-full flex" showThumbs={false}>
+                        <div className="flex-1 w-full h-full">Test</div>
+                        <div className="flex-1 w-full h-full">Test</div>
+                        <div className="flex-1 w-full h-full">Test</div>
+                        <div className="flex-1 w-full h-full">Test</div>
+                     </Carousel>
                   </div>
                   {data.map((item, i)=> (
                      <ProjectCard 
