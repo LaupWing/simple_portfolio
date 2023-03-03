3f3b91b37ff2c1280d4cf284db954e8223313f37
LaupWing <loc_strict@hotmail.com>
2023-03-03 19:10:47 +0100

    udpate

--- src/components/sections/SlideShow/Slide1Github.tsx
+++ src/components/sections/SlideShow/Slide1Github.tsx
@@ -1,5 +1,16 @@
+import { GetServerSideProps } from "next"
+
 export const Slide1Github = () => {
    return (
       <div className="flex-1 bg-indigo-400 m-2">Github</div>
    )
+}
+
+
+export const getServerSideProps:GetServerSideProps = async () => {
+   return {
+      props: {
+         test: ""
+      }
+   }
 }
