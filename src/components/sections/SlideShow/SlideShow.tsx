616b5ef5b39f119c3b7435c4faf401e1a934724f
LaupWing <loc_strict@hotmail.com>
2023-03-03 19:06:34 +0100

    update

--- src/components/sections/SlideShow/SlideShow.tsx
+++ src/components/sections/SlideShow/SlideShow.tsx
@@ -1,3 +1,24 @@
+import { Carousel } from "react-responsive-carousel"
+
 export const SlideShow = () => {
-   return <div>SlideShow</div>
+   return (
+      <div className="col-span-2 h-40 relative overflow-hidden rounded-2xl flex">
+         <img 
+            className="absolute inset-0 object-cover -z-10" 
+            src="https://images.pexels.com/photos/249798/pexels-photo-249798.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            alt="github" 
+         />
+         <div className="inset-0 absolute bg-white/50 backdrop-blur-sm -z-10"/>
+         <Carousel 
+            className="flex-1 h-full flex" 
+            showThumbs={false}
+            showStatus={false}
+         >
+            <div className="flex-1 bg-indigo-400 m-2">Test</div>
+            <div className="flex-1">Test</div>
+            <div className="flex-1">Test</div>
+            <div className="flex-1">Test</div>
+         </Carousel>
+      </div>
+   )
 }
