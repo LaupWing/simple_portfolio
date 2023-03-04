9c9cb7a2ad09b01a571a1ce7cacc2cf805b899e9
LaupWing <loc_strict@hotmail.com>
2023-03-04 16:19:37 +0100

    udpate

---
+++ simple-portfolio/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'simple portfolio',
+
+  projectId: 'rhegrw2z',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
