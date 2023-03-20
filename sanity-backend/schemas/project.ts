f226fda17a35b1d294741e5a36f36fb604b54b46
LaupWing <loc_strict@hotmail.com>
2023-03-20 18:57:50 +0100

    udpate

---
+++ sanity-backend/schemas/project.ts
@@ -0,0 +1,20 @@
+import { defineField, defineType } from "sanity"
+
+
+export default defineType({
+   name: "project",
+   title: "Project",
+   type: "document",
+   fields: [
+      defineField({
+         name: "title",
+         title: "Title",
+         type: "string"
+      }),
+      defineField({
+         name: "content",
+         title: "content",
+         type: "blockContent"
+      })
+   ]
+})
