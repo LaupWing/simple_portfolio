d3f13de649ed35ebb0b1f06b0c82b9c621a34a82
LaupWing <loc_strict@hotmail.com>
2023-03-20 19:11:13 +0100

    udpate

--- sanity-backend/schemas/project.ts
+++ sanity-backend/schemas/project.ts
@@ -32,11 +32,103 @@ export default defineType({
          type: "datetime"
       }),
       defineField({
-         name: "Image",
-         title: "Image",
+         name: "thumbnail",
+         title: "Thumbnail",
          type: "image",
          options: {
             hotspot: true,
+         },
+      }),
+      defineField({
+         name: "images",
+         title: "Images",
+         type: "array",
+         of:[
+            {
+               type: "image"
+            }
+         ],
+         description: "Project images"
+      }),
+      defineField({
+         name: "credentials",
+         title: "Credentials",
+         type: "boolean",
+         description: "Is credentials needed?"
+      }),
+      defineField({
+         name: "username",
+         title: "Username",
+         type: "string",
+         description: "Demonstration account's username",
+         hidden: ({document}) => !document?.credentials
+      }),
+      defineField({
+         name: "password",
+         title: "Password",
+         type: "string",
+         description: "Demonstration account's password",
+         hidden: ({document}) => !document?.credentials
+      }),
+      defineField({
+         name: "url",
+         title: "Url",
+         type: "url",
+         description: "URL of your website"
+      }),
+      defineField({
+         name: "skills",
+         title: "Skills",
+         type: "array",
+         description: "The skills you needed to build this project",
+         of: [
+            {
+               type: "string",
+            },
+         ],
+         options: {
+            list: [
+               {
+                  title: "Firebase", 
+                  value: "firebase"
+               },
+               {
+                  title: "React", 
+                  value: "react"
+               },
+               {
+                  title: "Vue", 
+                  value: "vue"
+               },
+               {
+                  title: "Solidity", 
+                  value: "solidity"
+               },
+               {
+                  title: "Nextjs", 
+                  value: "nextjs"
+               },
+               {
+                  title: "Typescript", 
+                  value: "typescript"
+               },
+               {
+                  title: "Wordpress", 
+                  value: "wordpress"
+               },
+               {
+                  title: "Gatsby", 
+                  value: "gatsby"
+               },
+               {
+                  title: "Tailwind", 
+                  value: "tailwind"
+               },
+               {
+                  title: "Css", 
+                  value: "css"
+               },
+            ],
          },
       }),
    ]
