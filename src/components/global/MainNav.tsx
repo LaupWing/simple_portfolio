a434558910782db67185fb6a30af81cfde89369e
LaupWing <loc_strict@hotmail.com>
2023-03-21 17:20:24 +0100

    update

--- src/components/global/MainNav.tsx
+++ src/components/global/MainNav.tsx
@@ -42,16 +42,24 @@ export const MainNav = () => {
       },
     }
    return (
-      <motion.header 
-         className="w-full text-sm flex py-6 items-center justify-between mx-auto max-w-4xl"
-         initial="hidden"
-         animate="visible"
-         variants={list}
-      >
-         <div className="flex items-center space-x-1">
+      <header className="w-full text-sm flex py-6 items-center justify-between mx-auto max-w-4xl">
+         <motion.div 
+            className="flex items-center space-x-1"
+            animate={{
+               x: 0,
+               opacity: 1,
+               transition: {
+                  duration: 1
+               }
+            }}
+            initial={{
+               x: -100,
+               opacity: 0
+            }}
+         >
             <div className="w-5 h-5 bg-indigo-500 rounded-full" />
             <h1 className="uppercase font-bold">Logo</h1>
-         </div>
+         </motion.div>
          <nav className="space-x-10 text-neutral-800">
             {links.map((link, i) => (
                <NavLink
@@ -61,7 +69,7 @@ export const MainNav = () => {
                />
             ))}
          </nav>
-      </motion.header>
+      </header>
    )
 }
 
