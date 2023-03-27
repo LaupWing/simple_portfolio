d6b1fe61dcf728becedb72c0d758e40880c08504
LaupWing <loc_strict@hotmail.com>
2023-03-27 15:06:27 +0200

    update

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,9 +1,9 @@
-import "dotenv/config"
 import { createClient } from "@sanity/client"
 
 export const client = createClient({
    projectId: "rhegrw2z",
    dataset: "production",
    apiVersion: "1",
-   useCdn: true
+   useCdn: true,
+   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
