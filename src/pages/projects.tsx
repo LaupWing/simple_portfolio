b1c2970139f5f6dc967a876849271d5c540bb63b
LaupWing <loc_strict@hotmail.com>
2023-04-09 16:13:53 +0200

    okay

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -29,7 +29,7 @@ const ProjectsPage:NextPage<ProjectPageProps> = ({
          setActiveSkills((prev) => [...prev, skill])
       }
    }
-   console.log(projects)
+   
    useEffect(() => {
       const query:Partial<Record<SkillsType[number], boolean>> = {}
       activeSkills.forEach(_skill => {
@@ -49,15 +49,45 @@ const ProjectsPage:NextPage<ProjectPageProps> = ({
       }
    }
    const activeProjects = projects.filter(project => activeSkills.find(x => project.skills.includes(x)) )
+   const container = {
+      hidden: {
+         opacity: 0
+      },
+      show: {
+         opacity: 1,
+         transition: {
+            staggerChildren: 0.1,
+            delayChildren: 0.6,
+         },
+      },
+   }
+   const item = {
+      hidden: { scale: 0, y: "60%" },
+      show: { scale: 1, y: 0 },
+   }
 
    return (
       <>
-         <div className="flex flex-col w-full rounded p-4 bg-slate-100 border-2 border-slate-200">
+         <motion.div 
+            className="flex flex-col w-full rounded p-4 bg-slate-100 border-2 border-slate-200"
+            variants={container}
+            initial="hidden"
+            animate="show"
+         >
             <p className="mb-3 text-slate-300 pl-1 text-xs font-bold uppercase">I've used the technologies below, but not all projects are listed here.</p>
             <div className="flex gap-x-4">
-               <button 
+               <motion.button 
+                  initial={{
+                     scaleX: 0,
+                  }}
+                  animate={{
+                     scaleX: 1,
+                     transition: {
+                        delay: 0.15 * config.skills.length
+                     }
+                  }}
                   className={clsx(
-                     "text-xs border-2 duration-200 rounded border-indigo-500 uppercase font-bold px-4",
+                     "text-xs origin-left border-2 duration-200 rounded border-indigo-500 uppercase font-bold px-4",
                      isAllActive
                         ? "text-white bg-indigo-500"
                         : "text-indigo-500 bg-transparent"
@@ -65,21 +95,25 @@ const ProjectsPage:NextPage<ProjectPageProps> = ({
                   onClick={toggle}
                >
                   Toggle
-               </button>
+               </motion.button>
                {config.skills.map(skill => (
-                  <Skill 
-                     skill={skill}
-                     size={30}
+                  <motion.button
+                     variants={item}
                      key={skill}
-                     className={clsx(
-                        "cursor-pointer duration-200",
-                        !activeSkills.includes(skill) ? "text-slate-300" : "text-indigo-500" 
-                     )}
-                     onClick={() => toggleSkill(skill)}
-                  />
+                  >
+                     <Skill 
+                        skill={skill}
+                        size={30}
+                        className={clsx(
+                           "cursor-pointer duration-200",
+                           !activeSkills.includes(skill) ? "text-slate-300" : "text-indigo-500" 
+                        )}
+                        onClick={() => toggleSkill(skill)}
+                     />
+                  </motion.button>
                ))}
             </div>
-         </div>
+         </motion.div>
          <section className="grid md:grid-cols-2 grid-cols-1 gap-6 overflow-hidden">
             {activeProjects.map((project, i)=> (
                <ProjectCard 
