e706339574a09b1d6e9dd0d737434d7a7646a374
LaupWing <loc_strict@hotmail.com>
2023-04-09 12:16:17 +0200

    skills in skil

---
+++ src/components/elements/Skill.tsx
@@ -0,0 +1,32 @@
+import { 
+   IconFirebase, 
+   IconGatsby, 
+   IconLaravel, 
+   IconNextjs, 
+   IconReact, 
+   IconShopify, 
+   IconSolidity, 
+   IconTailwind, 
+   IconTypescript, 
+   IconVue, 
+   IconWordpress 
+} from "~/components/elements"
+
+export const Skill = () => {
+   const skills = {
+      firebase: IconFirebase,
+      gatsby: IconGatsby,
+      laravel: IconLaravel,
+      nextjs: IconNextjs,
+      react: IconReact,
+      shopify: IconShopify,
+      solidity: IconSolidity,
+      tailwind: IconTailwind,
+      typescript: IconTypescript,
+      vue: IconVue,
+      wordpress: IconWordpress,
+   }
+   return (
+      <div>Skill</div>
+   )
+}
