0b8712befddf9a4eb28485157356faaa38b88e19
LaupWing <loc_strict@hotmail.com>
2023-04-10 15:09:38 +0200

    block

--- src/components/sections/BlockContent.tsx
+++ src/components/sections/BlockContent.tsx
@@ -32,12 +32,15 @@ const components: PortableTextComponents = {
    marks: {
    },
    block: {
-      h2: ({children}) => <h2 className="text-3xl mb-2">{children}</h2>,
-      h3: ({children}) => <h3 className="text-2xl mb-2">{children}</h3>,
-      normal: ({children}) => <p className="mb-2">{children}</p>
+      h2: ({children}) => <h2 className="text-3xl mb-4">{children}</h2>,
+      h3: ({children}) => <h3 className="text-2xl mb-4">{children}</h3>,
+      normal: ({children}) => <p className="mb-4">{children}</p>
    },
    list: {
-      bullet: ({children}) => <ul className="mb-2">{children}</ul>
+      bullet: ({children}) => <ul className="mb-4 gap-y-1 flex flex-col">{children}</ul>
+   },
+   listItem: {
+      bullet: ({children}) => <li className="list-disc list-inside">{children}</li>
    }
 }
 
