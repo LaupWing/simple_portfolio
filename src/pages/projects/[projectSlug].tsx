5498472d185a18982fbc68668a587cfa443a1f56
LaupWing <loc_strict@hotmail.com>
2023-04-10 13:35:02 +0200

    udpate

--- src/pages/projects/[projectSlug].tsx
+++ src/pages/projects/[projectSlug].tsx
@@ -3,7 +3,7 @@ import { ProjectType } from "typings"
 import { client, urlFor } from "~/sanity"
 
 const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
-   
+   console.log(project)
    return (
       <>
          <header className="p-2 w-full grid grid-cols-2 gap-6 ">
@@ -12,11 +12,14 @@ const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
                src={urlFor(project.thumbnail).url()} 
                alt="Image" 
             />
-            <div className="aspect-square rounded-2xl overflow-hidden">
+            <div className="aspect-square relative rounded-2xl overflow-hidden">
                <img
                   src={urlFor(project.thumbnail).url()} 
                   alt="Image" 
-               /> 
+               />
+               <div className="inset-0 p-4 absolute bg-slate-100/30 backdrop-blur">
+                  <h2 className="text-slate-900 font-bold text-2xl">{ project.name }</h2>
+               </div>
             </div>
          </header>
       </>
