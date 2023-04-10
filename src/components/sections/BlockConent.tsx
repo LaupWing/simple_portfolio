249a67e137e17242f7edb7922987aa341b681969
LaupWing <loc_strict@hotmail.com>
2023-04-10 14:40:08 +0200

    udpate

--- src/components/sections/BlockConent.tsx
+++ src/components/sections/BlockConent.tsx
@@ -1,5 +1,44 @@
-export const BlockConent = () => {
+import type { PortableTextComponents } from "@portabletext/react"
+import { getImageDimensions } from "@sanity/asset-utils"
+import { FC } from "react"
+import { urlFor } from "~/sanity"
+import { PortableText } from "@portabletext/react"
+
+const ImageComponent:FC<any> = ({value, isInline}) => {
+   const {width, height} = getImageDimensions(value)
+
    return (
-      <div>BlockConent</div>
+      <img 
+         src={urlFor(value)
+            .width(isInline ? 100 : 800)
+            .fit("max")
+            .auto("format")
+            .url()} 
+         style={{
+            display: isInline ? "inline-block" : "block", 
+            aspectRatio: width / height
+         }}
+         alt={value.alt || ""} 
+         loading="lazy"
+      />
+   )
+}
+
+const components: PortableTextComponents = {
+   types: {
+      image: ImageComponent
+   },
+   marks: {
+
+   }
+}
+
+export const BlockConent:FC<{
+   input: any[]
+}> = ({
+   input
+}) => {
+   return (
+      <PortableText value={input} components={components} />
    )
 }
