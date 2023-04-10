992c037431fb5c3e08a32b355361711866f007ed
LaupWing <loc_strict@hotmail.com>
2023-04-10 13:31:50 +0200

    udpate

--- src/pages/projects/[projectSlug].tsx
+++ src/pages/projects/[projectSlug].tsx
@@ -1,15 +1,23 @@
 import { GetServerSideProps, NextPage } from "next"
-import { useRouter } from "next/router"
 import { ProjectType } from "typings"
-import { client } from "~/sanity"
+import { client, urlFor } from "~/sanity"
 
 const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
-   const router = useRouter()
-   console.log(project)
+   
    return (
       <>
-         <header className="p-2 w-full bg-slate-200 rounded">
-            <h2>{project.name}</h2>
+         <header className="p-2 w-full grid grid-cols-2 gap-6 ">
+            <img 
+               className="aspect-square rounded-2xl" 
+               src={urlFor(project.thumbnail).url()} 
+               alt="Image" 
+            />
+            <div className="aspect-square rounded-2xl overflow-hidden">
+               <img
+                  src={urlFor(project.thumbnail).url()} 
+                  alt="Image" 
+               /> 
+            </div>
          </header>
       </>
    )
