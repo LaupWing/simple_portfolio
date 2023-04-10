dcbebd9d948c95fa93492fba8facd7c1d0a32c1d
LaupWing <loc_strict@hotmail.com>
2023-04-10 16:18:37 +0200

    base

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
+import { SocialType } from "typings"
+import { FC } from "react"
+import { IconDribbble, IconGithub, IconInstagram, IconLinkedIn } from "~/components/elements"
+import { IconType } from "react-icons"
+
 const AboutPage = () => {
-   
+   const socials: SocialType[] = [
+      "linkedin",
+      "github",
+      "dribble",
+      "instagram",
+   ]
+
    return (
       <>
          <section className="w-full grid grid-cols-3 gap-6 overflow-hidden">
@@ -27,8 +38,82 @@ const AboutPage = () => {
                />
                <div className="absolute inset-0 bg-white/40 backdrop-blur-3xl -z-10" />
             </div>
+            <div className="col-span-3 flex gap-6">
+               <div className="space-y-4 flex p-4 rounded-2xl flex-col bg-slate-200 border-slate-300 border-2">
+                  {socials.map((social) => (
+                     <Social
+                        key={social}
+                        name={social}
+                     />
+                  ))}
+               </div>
+               <div className="flex-1 border-2 p-4 border-slate-300 bg-slate-200 rounded-2xl">
+                  <h3 className="uppercase text-sm font-bold text-slate-500">General skills</h3>
+                  <div className="flex flex-col gap-y-4 text-xs mt-4">
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400 w-32">Communcation</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[80%]"/>
+                        </div>
+                     </div>
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400  w-32">Coding</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[90%]"/>
+                        </div>
+                     </div>
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400  w-32">Consistency</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[90%]"/>
+                        </div>
+                     </div>
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400  w-32">Dutch</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[90%]"/>
+                        </div>
+                     </div>
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400  w-32">English</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[90%]"/>
+                        </div>
+                     </div>
+                     <div className="flex items-center">
+                        <span className="font-bold uppercase text-slate-400  w-32">Fitness</span>
+                        <div className="flex-1 border-2 p-0.5 border-indigo-500 rounded-full">
+                           <div className="h-1.5 bg-indigo-500 rounded-full w-[90%]"/>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </section>
       </>
    )
 }
 export default AboutPage
+
+interface SocialProps {
+   name: SocialType
+}
+const Social:FC<SocialProps> = ({
+   name,
+}) => {
+   const socials:Record<SocialType, any> = {
+      instagram: IconInstagram,
+      linkedin: IconLinkedIn,
+      dribble: IconDribbble,
+      github: IconGithub
+   }
+   const IconComponent:IconType = socials[name]
+
+   return (
+      <div className="w-12 h-12 bg-white rounded-full text-neutral-900 flex items-center justify-center hover:text-indigo-500 cursor-pointer duration-200">
+         <IconComponent 
+            size={24}
+         />
+      </div>
+   )
+}
