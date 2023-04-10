07790617792b7d8c870dce3e5995a798858c65b3
LaupWing <loc_strict@hotmail.com>
2023-04-10 15:26:43 +0200

    udapte

--- src/pages/projects/[projectSlug].tsx
+++ src/pages/projects/[projectSlug].tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion"
 import { GetServerSideProps, NextPage } from "next"
 import { useState } from "react"
 import { Carousel } from "react-responsive-carousel"
@@ -30,11 +31,16 @@ const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
                ))}
             </Carousel>
             <div className="aspect-square relative rounded-2xl overflow-hidden">
-               <img
-                  src={urlFor(currentImage).url()} 
-                  alt="Image" 
-                  className="flex-1 h-full w-full object-cover" 
-               />
+               <AnimatePresence>
+                  <motion.img
+                     src={urlFor(currentImage).url()} 
+                     key={urlFor(currentImage).url()} 
+                     alt="Image" 
+                     className="flex-1 h-full w-full object-cover"
+                     animate={{ opacity: 1 }}
+                     exit={{ opacity: 0 }}
+                  />
+               </AnimatePresence>
                <div className="inset-0 p-6 absolute flex flex-col bg-slate-100/70 backdrop-blur">
                   <span className="font-bold text-sm text-slate-900/50">{ project.createdAt }</span>
                   <h2 className="text-slate-900 font-bold text-xl">{ project.name }</h2>
