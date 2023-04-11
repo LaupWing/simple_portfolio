4fb327fdbd2c92ae8fd74790227e4f739ec47b6e
LaupWing <loc_strict@hotmail.com>
2023-04-11 11:05:16 +0200

    stripped project

--- src/pages/projects/[projectSlug].tsx
+++ src/pages/projects/[projectSlug].tsx
@@ -1,7 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { GetServerSideProps, NextPage } from "next"
 import { useRouter } from "next/router"
-import { useState } from "react"
+import { FC, useState } from "react"
 import { Carousel } from "react-responsive-carousel"
 import { ProjectType } from "typings"
 import { IconExternal, IconGithub, Skill } from "~/components/elements"
@@ -49,40 +49,10 @@ const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
                   ))}
                </Carousel>
             </motion.div>
-            <div className="aspect-square shadow relative rounded-2xl overflow-hidden">
-               <AnimatePresence>
-                  <motion.img
-                     src={urlFor(currentImage).url()} 
-                     key={urlFor(currentImage).url()} 
-                     alt="Image" 
-                     className="flex-1 h-full w-full object-cover"
-                     animate={{ opacity: 1 }}
-                     exit={{ opacity: 0 }}
-                  />
-               </AnimatePresence>
-               <div className="inset-0 p-6 absolute flex flex-col bg-slate-100/80 backdrop-blur">
-                  <span className="font-bold text-sm text-slate-900/50">{ project.createdAt }</span>
-                  <h2 className="text-slate-900 font-bold text-xl">{ project.name }</h2>
-                  <div className="flex gap-x-4 my-2 text-indigo-600">
-                     {project.skills.map(skill => (
-                        <Skill 
-                           key={skill}
-                           skill={skill}
-                           size={30}
-                        />
-                     ))}
-                  </div>
-                  <p className="text-sm text-slate-900">{project.description}</p>
-                  <div className="flex gap-x-4 mt-auto">
-                     <button className="w-20 flex justify-center items-center bg-indigo-500 rounded py-0.5 text-indigo-900 border-indigo-600 border-2 shadow">
-                        <IconGithub size={24} />
-                     </button>
-                     <button className="w-20 flex justify-center items-center bg-indigo-500 rounded py-0.5 text-indigo-900 border-indigo-600 border-2 shadow">
-                        <IconExternal size={24} />
-                     </button>
-                  </div>
-               </div>
-            </div>
+            <ProjectInfo 
+               currentImage={currentImage}
+               project={project}
+            />
          </header>
          <div className="w-full p-6 bg-slate-100 border-2 border-slate-200 rounded-2xl shadow">
             <BlockContent value={project.content} />
@@ -91,6 +61,51 @@ const ProjectDetail:NextPage<{project: ProjectType}> = ({ project }) => {
    )
 }
 export default ProjectDetail
+
+const ProjectInfo:FC<{
+   currentImage: ProjectType["thumbnail"],
+   project: ProjectType
+}> = ({
+   currentImage,
+   project
+}) => {
+   return (
+      <div className="aspect-square shadow relative rounded-2xl overflow-hidden">
+         <AnimatePresence>
+            <motion.img
+               src={urlFor(currentImage).url()} 
+               key={urlFor(currentImage).url()} 
+               alt="Image" 
+               className="flex-1 h-full w-full object-cover"
+               animate={{ opacity: 1 }}
+               exit={{ opacity: 0 }}
+            />
+         </AnimatePresence>
+         <div className="inset-0 p-6 absolute flex flex-col bg-slate-100/80 backdrop-blur">
+            <span className="font-bold text-sm text-slate-900/50">{ project.createdAt }</span>
+            <h2 className="text-slate-900 font-bold text-xl">{ project.name }</h2>
+            <div className="flex gap-x-4 my-2 text-indigo-600">
+               {project.skills.map(skill => (
+                  <Skill 
+                     key={skill}
+                     skill={skill}
+                     size={30}
+                  />
+               ))}
+            </div>
+            <p className="text-sm text-slate-900">{project.description}</p>
+            <div className="flex gap-x-4 mt-auto">
+               <button className="w-20 flex justify-center items-center bg-indigo-500 rounded py-0.5 text-indigo-900 border-indigo-600 border-2 shadow">
+                  <IconGithub size={24} />
+               </button>
+               <button className="w-20 flex justify-center items-center bg-indigo-500 rounded py-0.5 text-indigo-900 border-indigo-600 border-2 shadow">
+                  <IconExternal size={24} />
+               </button>
+            </div>
+         </div>
+      </div>
+   )
+}
 
 export const getServerSideProps:GetServerSideProps = async ({ params }) => {
    const projectQuery = "*[_type == 'projects' && slug.current == $slug][0]"
