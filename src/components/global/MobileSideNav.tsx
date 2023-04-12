31cde00247c70b74dbca7ba050d0bd0f332456cd
LaupWing <loc_strict@hotmail.com>
2023-04-12 08:57:56 +0200

    udpate

--- src/components/global/MobileSideNav.tsx
+++ src/components/global/MobileSideNav.tsx
@@ -1,5 +1,13 @@
 export const MobileSideNav = () => {
    return (
-      <div>MobileSideNav</div>
+      <div className="fixed inset-0 bg-white z-50 w-screen h-screen p-10">
+         <header className="flex">
+
+            <h2 className="uppercase font-bold text-slate-700 text-xl tracking-wider flex items-center">
+               <div className="w-5 h-5 bg-indigo-500 rounded-full mr-1" />
+               MENU
+            </h2>
+         </header>
+      </div>
    )
 }
