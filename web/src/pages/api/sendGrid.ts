f5898a4d66a17c57918a18f2fff28deb2a465cd8
LaupWing <loc_strict@hotmail.com>
2023-04-21 12:09:41 +0200

    sendgrid

--- web/src/pages/api/sendGrid.ts
+++ web/src/pages/api/sendGrid.ts
@@ -1,12 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import sendgrid from "@sendgrid/mail"
 
 type ResponseData = {
    message: string
 }
 
-export default function handler(
+sendgrid.setApiKey(process.env.SEND_GRID!)
+
+export default async function handler(
    req: NextApiRequest,
    res: NextApiResponse<ResponseData>
 ) {
+   try{
+      await sendgrid.send({
+         to: "laupwing@gmail.com",
+         from: "laupwing@hotmail.com",
+         subject: req.body.subject,
+         html: "<div>You've got a mail</div>"
+      })
+   }catch(err) {
+      return res.status(500)
+   }
+
    return res.status(200).json({ message: ""})
 }
