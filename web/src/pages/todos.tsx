308bd00ac0196232d73740b824a6861b40f73be4
LaupWing <loc_strict@hotmail.com>
2023-04-27 16:58:09 +0200

    udpate

--- web/src/pages/todos.tsx
+++ web/src/pages/todos.tsx
@@ -4,17 +4,17 @@ import { GetServerSideProps } from "next"
 import { client } from "~/sanity"
 
 const TodosPage = ({
-   todos
+   // todos
 }) => {
-   console.log(todos)
+   // console.log(todos)
    return (
       <>
          <section className="w-full grid grid-cols-1 gap-6 overflow-hidden">
-            <ul className="col-span-full grid gap-2">
+            {/* <ul className="col-span-full grid gap-2">
                {todos.map(todo => (
                   <li className="bg-indigo-50 p-2 py-1 rounded border-indigo-100 border-2">test</li>
                ))}
-            </ul>
+            </ul> */}
          </section>
       </>
    )
